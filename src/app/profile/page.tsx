1510a6960aaa84bc00ba980f5b9ac8dd2a056ff5
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-09-20 12:04:51 +0400

    start custom signIn v1.2

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,15 +2,15 @@ import { getServerSession } from "next-auth";
 import { authConfig } from "../../../configs/auth";
 import Image from "next/image";
 
-
-
 export default async function Profile() {
   const session = await getServerSession(authConfig);
 
   return (
-    <><div>
-      <h1>Profile of {session?.user?.name}</h1>
-      {session?.user?.image && <Image src={session.user.image} width={200} height={200} alt="alt" />}
-    </div></>
+    <>
+      <div>
+        {session?.user?.image && <img src={session?.user?.image} alt="" />}
+        <h1>Profile of {session?.user?.name}</h1>
+      </div>
+    </>
   );
 }
