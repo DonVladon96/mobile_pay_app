de47d6f7e15c926d6cd462f1fb0516c1f47a7970
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-09-21 14:01:17 +0400

    change Home Page, about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,15 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import AboutMe from '@/component/AboutMe/AboutMe'
 import GTA from '../../../public/GTA_2314641931.png'
+import Link from "next/link";
 
 const About = () => {
   
   return (
     <div className={styles.container}>
       <AboutMe/>
+      <h1>Это приложение написано на Next Js + Type Script, а так же Backend база данных на Mongo DB</h1>
+      <br />
       <div className={styles.imgContainer}>
         <Image
           src={GTA}
@@ -17,10 +20,10 @@ const About = () => {
           className={styles.img}
         />
         <div className={styles.imgText}>
-          <h1 className={styles.imgTitle}>Немного креатива)))<br />Похож на парня сверху? Нет?</h1>
-          <h2 className={styles.imgDesc}>
+          <h2 className={styles.imgTitle}>Немного креатива)))<br />Похож на парня сверху? Нет?</h2>
+          <h3 className={styles.imgDesc}>
             А  так меня видит меня нейросеть по примеру игры Grand Theft Auto (GTA vice city)
-          </h2>
+          </h3>
         </div>
       </div>
       
@@ -28,16 +31,20 @@ const About = () => {
       <div className={styles.textContainer}>
       
         <div className={styles.item}>
-          <h1 className={styles.title}>Why me? Because I create...</h1>
+        
+          <h3 className={styles.title}>Why me? Because I create...</h3>
           <p className={styles.desc}>
-            <br />
             <br /> - Dynamic Websites
             <br />
             <br /> - Fast and Handy
             <br />
             <br /> - Mobile Apps
           </p>
-          <Button url="/contact" text="Contact" />
+          <Link href="/portfolio" className={styles.linkBtn}>
+          <button className={styles.BtnGoProfile}>
+           View Portfolio
+          </button>
+        </Link>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from '../../public/hero.png'
+import Hero from "../../public/acetone-2023921-133318-608.png";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Добро пожаловать на мою личную страничку</h1>
+        <h1 className={styles.title}>
+          Добро пожаловать на мою личную страничку
+        </h1>
         <p className={styles.desc}>
-         Здесь вы узнаете обо мне больше. После регистрации вас ждет сюрприз. А так же есть формочки для оплаты мобильной связи.
+          Здесь вы узнаете обо мне больше. После регистрации вас ждет сюрприз. А
+          так же есть формочки для оплаты мобильной связи.
         </p>
-        <Link href='/portfolio' className={styles.linkBtn}>
-        <button className={styles.BtnGoProfile}>Посмотреть другие работы</button>
-    </Link>
-        
+        <Link href="/portfolio" className={styles.linkBtn}>
+          <button className={styles.BtnGoProfile}>
+            Посмотреть другие работы
+          </button>
+        </Link>
       </div>
       <div className={styles.item}>
-        <Image src={Hero} alt={"Hero"} className={styles.img}/>
+        <Image src={Hero} alt={"Hero"} className={styles.img} />
       </div>
     </main>
   );
