b7c287a9aabf62866863e35ef1503c5fb6face15
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-10-02 13:14:31 +0400

    export error v1.0

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
     images:{
         domains: ['masterpiecer-images.s3.yandex.net', 'lh3.googleusercontent.com', 'img.shields.io']
-    }
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = __dirname;
+        return config;
+      },
 }
 
 
