87dae2c6ceea5fecde1346ec78bbf0414de8cc1b
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-10-03 15:53:51 +0400

    add stayles for operators

--- src/app/MobilePay/page.tsx
+++ src/app/MobilePay/page.tsx
@@ -4,33 +4,36 @@ import styles from "./page.module.css";
 const MobilePay = () => {
   return (
     <div className={styles.container}>
-      <h1>Выберите оператора</h1>
-      <Link href="/MobilePay/MTS">
+      <h1 className={styles.title}>Выберите оператора мобильной связи</h1>
+      <ul className={styles.navtab}>
+      <Link className={styles.navtab__button} href="/MobilePay/MTS">
         <span>MTS</span>
       </Link>
-      <Link href="/MobilePay/Megafon">
+      <Link className={styles.navtab__button} href="/MobilePay/Megafon">
         <span>Megafon</span>
       </Link>
 
-      <Link href="/MobilePay/Beeline">
+      <Link className={styles.navtab__button} href="/MobilePay/Beeline">
         <span>Beeline</span>
       </Link>
 
-      <Link href="/MobilePay/Tele2">
+      <Link className={styles.navtab__button} href="/MobilePay/Tele2">
         <span>Tele2</span>
       </Link>
 
-      <Link href="/MobilePay/Yota">
+      <Link className={styles.navtab__button} href="/MobilePay/Yota">
         <span>Yota</span>
       </Link>
 
-      <Link href="/MobilePay/Tinkoff">
+      <Link className={styles.navtab__button} href="/MobilePay/Tinkoff">
         <span>Tinkoff</span>
       </Link>
 
-      <Link href="/MobilePay/Sber">
+      <Link className={styles.navtab__button} href="/MobilePay/Sber">
         <span>Sber</span>
       </Link>
+      </ul>
+      
     </div>
   );
 };
