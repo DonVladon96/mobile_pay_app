a1cb8bb36670ac5db04ccc3d198a36015081a0a4
DonVladon96 <Petrov.Vladik2010@yandex.com>
2025-05-26 18:59:34 +0400

    wedding
    add wedding

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -1,8 +1,16 @@
+import WeAre from "../../../Image/public/gallery-4.jpg";
 export const items = {
 
     websites: [
       {
         id: 1,
+        title: "Wedding",
+        desc: "Приглашение на нашу свадьбу",
+        image: WeAre,
+        url: 'https://wedding-va.vercel.app'
+      },
+      {
+        id: 2,
         title: "Статичный сайт",
         desc: "Создание статичного сайта для тренировки верстки и БЭМ методологии",
         image:
@@ -10,7 +18,7 @@ export const items = {
         url: 'https://donvladon96.github.io/how-to-learnPablic/'
       },
       {
-        id: 2,
+        id: 3,
         title: "Путешествие по России",
         desc: "Настоящая страна не в выпусках новоcтей, а здесь. В данной работе я тренировал навыки адаптивной верстки для различных размеров экранов",
         image:
@@ -18,7 +26,7 @@ export const items = {
           url: 'https://donvladon96.github.io/russian-travel/'
       },
       {
-        id: 3,
+        id: 4,
         title: "Проект МЕСТО",
         desc: "Mesto: интерактивная страница, куда можно добавлять фотографии, удалять их и ставить лайки. Выполнен с использованием библиотеки React и разработкой Backend сервера на Node Js",
         image:
